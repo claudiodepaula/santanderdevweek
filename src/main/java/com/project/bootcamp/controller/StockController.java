231f8347679ac88864590492fa7818aff36a9450
Claudiomarcosep <claudiomarcosep@gmail.com>
2021-06-01 20:40:49 -0300

    CrossOrigin modification

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins=*)
 @RestController
 @RequestMapping(value = "/stock")
 
